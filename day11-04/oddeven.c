d5a351bad227372964991e2973ffccedf4f14f60
Anshupaija <paijaanshu223@gmail.com>
2024-04-12 15:41:01 +0530

    saved

---
+++ day11-04/oddeven.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int esum=0,osum=0;
+    for (int i = 1; i <=10; i++)
+    {
+        if (i%2==0)
+        {
+            esum+=i;
+        }
+        else
+        {
+            osum+=i;
+        }
+        
+    }
+    
+    if (osum>esum)
+    {
+        printf("%d",osum-esum);
+    }
+    else if(esum>osum)
+    {
+        printf("%d",esum-osum);
+    }
+    else{
+        printf("Zero(0)");
+    }
+    
+    return 0;
+}
