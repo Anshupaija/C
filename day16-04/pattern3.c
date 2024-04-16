f853239c6564c36579cdde1801ec8a114c090aeb
Anshupaija <paijaanshu223@gmail.com>
2024-04-16 16:04:26 +0530

    adfdfd

---
+++ day16-04/pattern3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    //For loop of Row-
+    for (int i = 1; i <= 4; i++)
+    {
+        //For loop of Column-
+        for (int j = 1; j <= 6; j++)
+        {
+            printf("%d",i);
+        }
+        //Print In New line-
+        printf("\n");
+
+    }
+}
---
+++ day16-04/pattern4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    //For loop of Row-
+    for (int i = 1; i <= 3; i++)
+    {
+        //For loop of Column-
+        for (int j = 1; j <= 5; j++)
+        {
+            printf("* ");
+        }
+        //Print In New line-
+        printf("\n");
+
+    }
+}
